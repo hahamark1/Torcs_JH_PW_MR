2dea5751833ab57ce4a6b8b963b00dbcfb029df8
jim3101 <jimhoekstra2@gmail.com>
2017-11-20 14:12:50 +0100

    read logs ready for use

--- read_log.py
+++ read_log.py
@@ -1,15 +1,26 @@
 import pickle
+import numpy as np
+import os
 
-filepath = "drivelogs/forza.pickle"
+files = []
+
+for file in os.listdir(os.path.join(os.getcwd() + '/drivelogs')):
+	if file.endswith(".pickle"):
+		files.append(os.path.join(os.path.join(os.getcwd() + '/drivelogs'), file))
+
 all_states = []
-with open(filepath, 'rb') as logfile:
-	unpickler = pickle.Unpickler(logfile)
-	try:
-		while True:
-			state, command = unpickler.load()
-			all_states.append(state)
-	except EOFError:
-		pass
+for filepath in files:
+	with open(filepath, 'rb') as logfile:
+		unpickler = pickle.Unpickler(logfile)
+		try:
+			while True:
+				state, command = unpickler.load()
+				all_states.append(state)
+		except EOFError:
+			pass
 
-# print(len(all_states))
-print(type(all_states[1]))
+inputs = np.zeros((len(all_states), len(all_states[1].distances_from_edge) + 1))
+outputs = np.zeros((len(all_states), 2))
+for index, state in enumerate(all_states):
+	outputs[index, :] = [state.speed_x, state.distance_from_center]
+	inputs[index, :] = list(state.distances_from_edge) + [state.angle]
